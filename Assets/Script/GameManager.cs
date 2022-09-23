b9579999833a7b316dc300dbf720400d29fb1343
SanaiJaemin <woals1566@gmail.com>
2022-09-23 17:49:26 +0900

    1

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,11 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    public InteractionUI objectsenser;
-    public GameObject WoodPref;
-    public GameObject HardPref;
-    public GameObject RockPref;
-    public GameObject DiamondPref;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -24,13 +20,7 @@ public class GameManager : MonoBehaviour
     {
         
     }
-    IEnumerator DeSetActive (GameObject Energy,float DestoryTime)
-    {
-        yield return null;
-
-        yield return new WaitForSeconds(DestoryTime);
-        Energy.SetActive(false);
-    }
+    
     
 
 
